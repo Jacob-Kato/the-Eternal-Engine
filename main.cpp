1a1a98ba879acc73e00165242c30b94438c0a44f
Jacob-Kato <jacobyala23@gmail.com>
2026-09-16 18:37:02 -0400

    refactor: changed from new to malloc because you can resize with it
    feat: added a size check and resizing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
 struct item {
   int value;
@@ -7,15 +9,33 @@ struct item {
 
 class Storage {
 private:
-  struct item *array = new struct item;
   int count = 0;
-  int size = 0;
+  int size = 2;
+  struct item *array;
 
 public:
-  Storage() {};
+  Storage() {
+    array = (struct item *)malloc((size * sizeof(struct item)));
+    if (array == NULL) {
+      std::cout << "Could not Allocate memory for array!!" << "\n";
+      return;
+    }
+  };
 
   void add_item(int value, int amount) {
     struct item new_item;
+
+    if (count == size) {
+      struct item *temp;
+      temp = (struct item *)realloc(temp, 2 * sizeof(struct item));
+      if (temp == NULL) {
+        std::cout << "Cound not Reallocate Array !!" << "\n";
+        return;
+      }
+      array = temp;
+      size *= 2;
+      free(temp);
+    }
 
     for (int i = 0; i < count; i++) {
       if (value == array[i].value) {
@@ -23,7 +43,6 @@ public:
         return;
       }
     }
-
     new_item.value = value;
     new_item.amount = amount;
     array[count] = new_item;
@@ -33,6 +52,7 @@ public:
     for (int i = 0; i < count; i++) {
       std::cout << "Value: " << array->value << "\n";
       std::cout << "Amount: " << array->amount << "\n";
+      std::cout << "Storage left: " << size - count << "\n";
       std::cout << "\n";
     }
   }
@@ -42,6 +62,7 @@ int main() {
   Storage myst;
   myst.add_item(2, 3);
   myst.add_item(2, 3);
+  myst.add_item(6, 3);
   myst.print_members();
   return 0;
 }
